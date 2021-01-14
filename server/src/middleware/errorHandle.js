54eea3cd6451f0397b1a41e1b1bade15871d396f
Cattttttttt <1067812695@qq.com>
2021-01-14 23:22:43 +0800

    Update errorHandle.js

--- server/src/middleware/errorHandle.js
+++ server/src/middleware/errorHandle.js
@@ -9,7 +9,6 @@ module.exports = async (ctx, next) => {
   try {
     await next()
   } catch (error) {
-    console.log(error)
     ctx.status = error.status || 403
     ctx.body = {
       message: error.message
